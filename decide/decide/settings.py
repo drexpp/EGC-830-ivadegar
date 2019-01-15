d0564822a3acab52d28de86352962e96d6f780c1
Iván De los Santos García <ivandega22@gmail.com>
2019-01-15 19:02:29 +0100

    Changes needed for the deployment of heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -68,7 +68,10 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://decide-examen-ivan.herokuapp.com'
+
+APIS = {}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -157,6 +160,9 @@ try:
     from local_settings import *
 except ImportError:
     print("local_settings.py not found")
+
+import django_heroku
+django_heroku.settings(locals())
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
